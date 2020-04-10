a9e02262cf1268644e09dcb5b260886c04795c3a
woshinisenbaba <42974737+woshinisenbaba@users.noreply.github.com>
2020-04-09 19:56:04 -0500

    Add files via upload

---
+++ Search.php
@@ -0,0 +1,46 @@
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Name</th><th>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b5819b447375bb";
+$password = "4bd1f395";
+$dbname = "heroku_da95d60c5d49de6";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT f.Name FROM food_dishes f, restaurant r WHERE f.RestaurantID = r.RestaurantID");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
