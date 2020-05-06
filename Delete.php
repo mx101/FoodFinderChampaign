b450e79c310bd4b38f4f101358f20bf46bde0975
woshinisenbaba <42974737+woshinisenbaba@users.noreply.github.com>
2020-05-06 14:29:38 -0500

    Add files via upload

---
+++ Delete.php
@@ -0,0 +1,32 @@
+<?php 
+session_start()
+include("config.php")
+
+
+$servername = "foodfinderdatabase.cftnyz75hjki.us-east-2.rds.amazonaws.com";
+$username = "admin";
+$password = "CS411uiuc";
+$dbname = "foodfinderdatabase";
+
+$User_ID = $_SESSION['login_id'];
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to delete a record
+    $sql = "DELETE FROM users WHERRE userID = $User_ID;'";
+
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Record deleted successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
